25e3b7510b61fc94ee72ea4939154a0db7c14678
deleonkf <deleonfaith814@gmail.com>
2022-02-22 13:04:43 -0600

    Set up proxy to connect frontend and backend

---
+++ web_app/text_savvy_backend/README.md
@@ -0,0 +1,19 @@
+# Text Savvy Server
+
+## Quick start
+1. Make sure `node` and `npm` are installed. To check:
+    ```
+    $ node -v
+    $ npm -v
+    ```
+    - Go to [https://nodejs.org/en/download/](https://nodejs.org/en/download/) to download the installer.
+2. Install dependencies.
+    ```
+    $ npm install
+    ```
+    - Update `npm` if necessary.
+3. Run the server.
+    ```
+    $ npm start
+    ```
+4. Visit [localhost:5000](http://localhost:5000) in Chrome. Port 5000 is where the Express server will be running on.
--- web_app/text_savvy_backend/server.js
+++ web_app/text_savvy_backend/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// console.log that the server is up and running
-app.listen(port, () => console.log('Listening on port ${port}'));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// GET method to test connection
+app.get('/api', (req, res) => {
+    res.send({ message: 'Express backend connected!' });
 });
+
+// console.log that the server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
