2c61d5a655110f5538981a57be48922710e3458e
mariellemanlulu <mariellem016@gmail.com>
2022-02-22 21:13:45 -0600

    Set up Auth0 with React

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,36 +1,24 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import App from "./App";
-import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from '@auth0/auth0-react';
 
-const App = () => {
-  const { isLoading, error } = useAuth0();
 
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
+function App() {
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
-    <Router history={history}>
-      <div id="app" className="d-flex flex-column h-100">
-   
-      
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/external-api" component={ExternalApi} />
-
-     
-
-      </div>
-    </Router>
+    <>
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile/>
+    </>
   );
 };
-
-
 
 
 export default App;
