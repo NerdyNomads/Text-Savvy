c0f6c8a9de942ad0aa0bcdf625328e9807c1a12a
Joshua Moreira <joshuamoreira3@gmail.com>
2022-02-24 01:46:43 -0600

    Added a notification when a menu item is clicked

--- web-app/public/eventPage.js
+++ web-app/public/eventPage.js
@@ -1,18 +1,67 @@
 // Figure out how to make it so the option only appears when there is text selected
 
+const workspaces = [
+    {
+        id: "1",
+        name: "Science Notes",
+    },
+    {
+        id: "2",
+        name: "Funny Quotes",
+    },
+    {
+        id: "3",
+        name: "Research Paper Notes",
+    },
+    {
+        id: "4",
+        name: "Places to Explore",
+    },
+];
+
 const parentContextMenuItem = {
     id: "parent",
-    title: "parent",
-    contexts: ["all"],
+    title: "Add to workspace",
+    contexts: ["selection"],
 };
 
 const childContextMenuItem = {
     id: "child",
     title: "child",
-    contexts: ["all"],
+    contexts: ["selection"],
     parentId: "parent",
 };
 
 chrome.contextMenus.create(parentContextMenuItem);
 
-chrome.contextMenus.create(childContextMenuItem);
+// Add all the workspaces as children
+workspaces.map(({ id, name }) =>
+    chrome.contextMenus.create({
+        id,
+        title: name,
+        contexts: ["selection"],
+        parentId: "parent",
+    })
+);
+
+// Add functionality when an item is clicked
+chrome.contextMenus.onClicked.addListener((clickData) => {
+    // Check if the menu item that was clicked was one of the workspaces
+    const matchingIds = workspaces.filter((workspace) => clickData.menuItemId === workspace.id);
+
+    if (matchingIds.length > 0) {
+        console.log("matchingIds is:", matchingIds);
+
+        chrome.notifications.create({
+            title: "Click Notifier",
+            message:
+                "You clicked [" +
+                matchingIds[0].name +
+                ']. The text is: "' +
+                clickData.selectionText +
+                '"',
+            iconUrl: "Text Savvy Logo.png",
+            type: "basic",
+        });
+    }
+});
