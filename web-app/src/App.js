d0fbe3b72d448034a7e99885427930f49563c0ab
Eman <valetee@myumanitoba.ca>
2022-02-24 01:23:26 -0600

    Fixed App.js error

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
@@ -21,42 +20,5 @@ function App() {
   );
 };
 
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-    const [data, setData] = React.useState(null);
-
-    // Test backend connection using GET route from server.js
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-
-                <p>{!data ? "Loading..." : data}</p>
-            </header>
-        </div>
-    );
-}
->>>>>>> FD/feat/setup-nodejs-express
 
 export default App;
