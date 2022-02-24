7ff9926e6a718b406498e1d6d76508bc5949ef92
Eman <valetee@myumanitoba.ca>
2022-02-24 01:47:52 -0600

    Resolved conflicts after merging NodeJs and React changes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Profile from "./components/Profile";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
 import { useAuth0 } from '@auth0/auth0-react';
 
-
 function App() {
+    const [data, setData] = React.useState(null);
+    const { isLoading } = useAuth0();
 
-  const { isLoading } = useAuth0();
+    // Test backend connection using GET route from server.js
+    React.useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
 
-  if (isLoading) return <div>Loading...</div>;
+    return (
+        <div className="App">
+            <header className="App-header">
 
-  return (
-    <>
-      <LoginButton/>
-      <LogoutButton/>
-      <Profile/>
-    </>
-  );
-};
-
+                <p>{!data ? "Loading..." : data}</p>
+                <LoginButton/>
+                <LogoutButton/>
+                <Profile/>
+            </header>
+        </div>
+    );
+}
 
 export default App;
