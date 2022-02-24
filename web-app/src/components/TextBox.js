34a4cc39d81261d28fafa7c1f7c5d8d2bba4af54
Eman <valetee@myumanitoba.ca>
2022-02-24 16:09:47 -0600

    Add titles to TextBoxes

--- web-app/src/components/TextBox.js
+++ web-app/src/components/TextBox.js
@@ -4,7 +4,8 @@ class TextBox extends React.Component {
     render() {
         return (
             <div className= "text-box">
-                It does not matter how fast you go as long as you do not stop.
+                <b>My Inspiration</b>
+                <div>It does not matter how fast you go as long as you do not stop.</div>
             </div>
         )
     }
