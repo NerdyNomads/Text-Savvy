d78b6d3a09f4e160b425c3a3a612432ff1be05c8
mariellemanlulu <mariellem016@gmail.com>
2022-02-25 12:06:38 -0600

    Rebased to develop to keep updated

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import LoginButton from "./components/LoginButton";
 import TextList from "./components/TextList";
 import Sidebar from "./components/Sidebar";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-    const [data, setData] = React.useState(null);
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
+	const [data, setData] = React.useState(null);
+	const { isAuthenticated } = useAuth0();
 
-    // Test backend connection using GET route from server.js
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
+	// Test backend connection using GET route from server.js
+	React.useEffect(() => {
+		fetch("/api")
+			.then((res) => res.json())
+			.then((data) => setData(data.message));
+	}, []);
 
-    if (!isAuthenticated) {
-        return <LoginButton/>
-    } 
-    else {
-    return (
-        <div className="App">
-            <header className="App-header">
-                    <p>{!data ? "Loading..." : data}</p>
-                    <TextList/>
-                    <Sidebar/>
-            </header>
-        </div>
-    )};
+	if (!isAuthenticated) {
+		return <LoginButton/>;
+	} 
+	else {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>{!data ? "Loading..." : data}</p>
+					<TextList/>
+					<Sidebar/>
+				</header>
+			</div>
+		);}
 }
 
 export default App;
