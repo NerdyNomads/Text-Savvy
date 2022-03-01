69b3cf9b8dfe42f8581fa696dedd814d28f7a605
mariellemanlulu <mariellem016@gmail.com>
2022-02-28 19:32:40 -0600

    add Textbox and TextList components

---
+++ front-end/src/organisms/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import LogoutButton from "../atoms/LogoutButton";
+import "./Sidebar.css";
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className= "sidebar">
+        <b>Welcome, Emmanuel!</b>
+        <div>
+                    Please Select a Workspace...
+        </div>
+        <LogoutButton/>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
