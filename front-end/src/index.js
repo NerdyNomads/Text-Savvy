9c4793ba2a4dea92eeaf20e18c49b06ac33173ab
Marielle Manlulu <manlulum@myumanitoba.ca>
2022-03-03 16:01:37 -0600

    remove landing page to redirect directly to login modal (#64)
    * remove landing page to redirect directly to login modal
    
    * Fix Auth0 user reference issue and updated useEffect
    
    Co-authored-by: valetee <66437477+valetee@users.noreply.github.com>
    Co-authored-by: deleonkf <deleonfaith814@gmail.com>

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -11,10 +11,17 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience= process.env.REACT_APP_AUTH0_AUDIENCE;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+    <Auth0Provider 
+      domain={domain} 
+      clientId={clientId} 
+      redirectUri={window.location.origin}
+      audience={audience}
+      useRefreshTokens={true}
+      cacheLocation="localstorage">
       <App />
     </Auth0Provider>
   </React.StrictMode>,
