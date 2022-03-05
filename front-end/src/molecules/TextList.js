e17975a256abeb1d04bb2ee0b3d0a900078bc8f5
r3ym3l <ereymel1@gmail.com>
2022-03-05 16:55:29 -0600

    Co-authored-by: Marielle Manlulu <manlulum@myumanitoba.ca>

--- front-end/src/molecules/TextList.js
+++ front-end/src/molecules/TextList.js
@@ -1,15 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
 
 import TextBox from "../atoms/TextBox";
 import TextBoxAdd from "../atoms/TextBoxAdd";
 import "./TextList.css";
 
-function TextList( {list} ) {
+import axios from "axios";
+
+function TextList() {
+  const [ dataIsLoaded, setDataIsLoaded ] = useState(false);
+  const [ textItems, setTextItems ] = useState(null);
+  // const [ texts, setTexts ] = useState([]);
+
+  const handleDelete = (id) => {
+    // delete item in JSON based on id
+    const newRenderedItem = textItems.filter(text => id !== text._id);
+    setTextItems(newRenderedItem);
+  };
+
+  async function getTexts() {
+    let result = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/texts`);
+    return result?.data;
+  }
+
+  useEffect(async () => {
+    let dbTexts = await getTexts();
+    setDataIsLoaded(true);
+    if (dataIsLoaded) {
+      setTextItems(dbTexts);
+    }
+  }, [dataIsLoaded]);
 
   const renderList = () => {
-    return list.map( (l) => {
-      return <TextBox key={l._id} textItem={l}/>;
+    return textItems && textItems.map( (l) => {
+      return <TextBox key={l._id} textItem={l} onDelete = {(id) => handleDelete(id)}/>;
     });
   };
 
@@ -21,8 +45,8 @@ function TextList( {list} ) {
   );
 }
 
-TextList.propTypes = {
-  list: PropTypes.array.isRequired
-};
+// TextList.propTypes = {
+//   list: PropTypes.array.isRequired
+// };
 
 export default TextList;
