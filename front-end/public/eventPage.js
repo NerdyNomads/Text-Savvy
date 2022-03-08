7ca34fff1108019f9421a6812ce9c443ebb43cee
mariellemanlulu <mariellem016@gmail.com>
2022-03-08 14:45:02 -0600

    minor changes on text formatting

--- front-end/public/eventPage.js
+++ front-end/public/eventPage.js
@@ -23,7 +23,7 @@ const workspaces = [
 const formatText = (input) => {
   
   input = input.slice(0, MAX_TEXT_NOTIF_CHARACTERS); 
-  return input.length > MAX_TEXT_NOTIF_CHARACTERS ? `"${input}..."` : input ;
+  return input.length > MAX_TEXT_NOTIF_CHARACTERS ? `"${input}..."` : `"${input}"` ;
 };
 
 const checkWorkspaceExistance = (id) => {
@@ -47,7 +47,7 @@ const createNotification = (text) => {
   chrome.notifications.create({
     title: "Text Saved",
     message:
-      `The text "${text}" has been added to your workspace.`,
+      `The text ${text} has been added to your workspace.`,
     iconUrl: "logo.png",
     type: "basic",
   });
