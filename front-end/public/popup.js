3ded411f09e78b17cef2a3b389adadd42384de61
mariellemanlulu <mariellem016@gmail.com>
2022-03-08 14:23:02 -0600

    condense onclck event code

--- front-end/public/popup.js
+++ front-end/public/popup.js
@@ -1,11 +1,5 @@
 /* global chrome */
 
 const button = document.getElementById("extButton");
-button.onclick = function click() {
-  chrome.tabs.create(
-    {
-      url: "http://localhost:3000"
-    },
-  );
-};
+button.onclick = () => chrome.tabs.create({ url: "http://localhost:3000" });
 
