9d5404a868deac961b11a40be01670877c8d21b5
mariellemanlulu <mariellem016@gmail.com>
2022-03-08 14:09:31 -0600

    Remove unneeded console log

--- front-end/public/popup.js
+++ front-end/public/popup.js
@@ -1,10 +1,11 @@
 /* global chrome */
+
 const button = document.getElementById("extButton");
 button.onclick = function click() {
-  console.log("Button clicked.");
   chrome.tabs.create(
     {
       url: "http://localhost:3000"
     },
   );
 };
+
