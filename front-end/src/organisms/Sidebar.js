e9fe2e8d0a871e261432e201abf591fcc6bfa9f2
mariellemanlulu <mariellem016@gmail.com>
2022-03-21 17:55:36 -0500

    reorder sidebar content #110

--- front-end/src/organisms/Sidebar.js
+++ front-end/src/organisms/Sidebar.js
@@ -51,7 +51,7 @@ function Sidebar() {
   //   }
   // ];
 
-  const { logout, user } = useAuth0();
+  const { logout } = useAuth0();
 
   const spacing = <div className="SideBar-spacing"/>;
   const divider = <div className="SideBar-divider"/>;
@@ -68,10 +68,16 @@ function Sidebar() {
         <div className={`${componentName}-logo-text`}>TextSavvy</div>
       </div>
       {divider}
-      {/* eslint-disable-next-line react/no-unescaped-entities */ }
-      <div className={`${componentName}-title`}>{user.nickname}'s Workspace</div>
+
+      <SidebarWorkspaceItem name="My Workspace"/>
+      <SidebarWorkspaceItem name="This Is a very long Name for a workspace"/>
+      <SidebarWorkspaceItem name="wokrpsace"/>
+      
       {dividerLight}
       {spacing}
+
+      
+
       <div className={`${componentName}-manageacc ${componentName}-option`} onClick={handleManageAccountOnClick}>
         <div className={`${componentName}-option-logo`}><GearSmallIcon/></div>
         <div className={`${componentName}-option-text`}>Manage Account</div>
@@ -81,9 +87,7 @@ function Sidebar() {
         <div className={`${componentName}-option-text`}>Logout</div>
       </div>
       
-      <SidebarWorkspaceItem name="My Workspace"/>
-      <SidebarWorkspaceItem name="This Is a very long Name for a workspace"/>
-      <SidebarWorkspaceItem name="wokrpsace"/>
+ 
     </div>
   );
 
