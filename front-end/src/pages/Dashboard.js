610a6d4afbcb229417c205136fb60c7c9e85a305
mariellemanlulu <mariellem016@gmail.com>
2022-03-23 18:22:35 -0500

    add-workspace-title

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,12 +1,34 @@
 import React, {useState, useEffect} from "react";
+import PropTypes from "prop-types";
 
 import axios from "axios";
 
 import TextList from "../molecules/TextList";
 import "./Dashboard.css";
 
-function Dashboard() {
+function Dashboard({ workspaceId }) {
   const [ textItems, setTextItems ] = useState(null);
+  const [ renderedWorkspaceTitle, setRenderedWorkspaceTitle ] = useState("");
+
+  const componentName = "Dashboard";
+
+  const getCurrentWorkspace = () => {
+    // FETCH CURRENT WORKSPACE based on id
+
+
+    const fakeWorkspace = {
+      id: "1",
+      name: "My Workspace"
+    };
+
+    setRenderedWorkspaceTitle(fakeWorkspace.name);
+  };
+
+  useEffect(() => {
+
+    getCurrentWorkspace(workspaceId);
+
+  }, []);
 
   /**
    * Grabs data from the db.
@@ -27,13 +49,17 @@ function Dashboard() {
   }, []);
 
   return (
-    <div className="Dashboard">
+    <div className={`${componentName}`}>
+      <div className={`${componentName}-title`}>{renderedWorkspaceTitle}</div>
       { textItems &&
           <TextList list={textItems}/>
       }
     </div>
   );
 }
+Dashboard.propTypes = {
+  workspaceId: PropTypes.string.isRequired,
+};
 
 
 export default Dashboard;
