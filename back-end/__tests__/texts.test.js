b78e0c72eb5d2cb6a2440e345e37a05358b3428b
deleonkf <deleonfaith814@gmail.com>
2022-03-31 13:21:38 -0500

    Fixed failing backend tests

--- back-end/__tests__/texts.test.js
+++ back-end/__tests__/texts.test.js
@@ -28,21 +28,22 @@ describe("Texts Router Tests", () => {
 	});
 
 	describe("POST /texts/add", () => {
-    	// test("Add a valid text to the database.", async () => {
-		// 	await request(app).post("/texts/add")
-		// 		.send(testText)
-		// 		.expect(200)
-		// 		.then((res) => {
-		// 			expect(res.statusCode).toEqual(200);
-		// 			expect(res.body).toEqual("Text added!");
-		// 		});
+    	test("Add a valid text to the database.", async () => {
+			await request(app).post("/texts/add")
+				.send(testText)
+				.expect(200)
+				.then((res) => {
+					expect(res.statusCode).toEqual(200);
+					expect(res.body.text).toEqual(testText.text);
+					expect(res.body.source).toEqual(testText.source);
+				});
 
-		// 	// Remove test data from DB
-		// 	await Text.findOneAndDelete({
-        //         text: testText.text,
-        //         source: testText.source
-        //     });
-    	// });
+			// Remove test data from DB
+			await Text.findOneAndDelete({
+                text: testText.text,
+                source: testText.source
+            });
+    	});
 
 		test("Add an invalid text to the database.", async () => {
 			await request(app).post("/texts/add")
