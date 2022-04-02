2d9e5e77071bff656cdaa76daa0fd94cf9a0cd27
r3ym3l <ereymel1@gmail.com>
2022-04-02 18:29:52 -0500

    Fixed merge conflict in SIdebar.test.js

--- front-end/src/__tests__/Sidebar.test.js
+++ front-end/src/__tests__/Sidebar.test.js
@@ -5,8 +5,13 @@ import { shallow } from "enzyme";
 
 import Sidebar from "../organisms/Sidebar";
 import { useAuth0 } from "@auth0/auth0-react";
+import SidebarWorkspace from "../molecules/SidebarWorkspace";
 
 jest.mock("@auth0/auth0-react"); 
+jest.mock("../molecules/SidebarWorkspace", () => {
+  const SidebarWorkspace = () => <div />;
+  return SidebarWorkspace;
+});
 
 beforeEach(() => {
   useAuth0.mockReturnValue({
@@ -24,9 +29,47 @@ test("Should match the snapshot.", () => {
 });
 
 test("Clicking the logout button should call Auth0's logout function.", () => {
+  global.chrome = {
+    runtime: {
+      sendMessage: jest.fn()
+    }
+  };
   const mockFunc = () => null;
   let wrapper = shallow(<Sidebar onClickWorkspace={mockFunc} accountId=""/>);
 
   let logoutDiv = wrapper.find(".SideBar-logout");
   expect(logoutDiv.length).toEqual(1);
+  logoutDiv.simulate("click");
+  expect(useAuth0().logout).toHaveBeenCalledTimes(1);
+});
+
+test("Should display the logout button.", () => {
+  const mockFunc = () => null;
+  let wrapper = shallow(<Sidebar onClickWorkspace={mockFunc} accountId=""/>);
+
+  let logoutDiv = wrapper.find(".SideBar-logout");
+  expect(logoutDiv.length).toEqual(1);
+
+  let logoutText = wrapper.find(".SideBar-option-text").at(1);
+  expect(logoutText.length).toEqual(1);
+  expect(logoutText.text()).toEqual("Logout");
+});
+
+test("Should display the 'Manage Account' button.", () => {
+  const mockFunc = () => null;
+  let wrapper = shallow(<Sidebar onClickWorkspace={mockFunc} accountId=""/>);
+
+  let manageAccDiv = wrapper.find(".SideBar-manageacc");
+  expect(manageAccDiv.length).toEqual(1);
+
+  let manageAccText = wrapper.find(".SideBar-option-text").first();
+  expect(manageAccText.length).toEqual(1);
+  expect(manageAccText.text()).toEqual("Manage Account");
+});
+
+test("Should display the SidebarWorkspace component.", () => {
+  const mockFunc = () => null;
+  let wrapper = shallow(<Sidebar onClickWorkspace={mockFunc} accountId=""/>);
+  
+  expect(wrapper.containsMatchingElement(<SidebarWorkspace />)).toEqual(true);
 });
