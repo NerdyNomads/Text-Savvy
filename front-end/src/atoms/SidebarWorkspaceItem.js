889f75a1734aca124b656f4cc61e050d26c3e0a7
Joshua Moreira <joshuamoreira3@gmail.com>
2022-04-02 19:39:48 -0500

    Changed the workpsace edit icon from a pencil to a gear

--- front-end/src/atoms/SidebarWorkspaceItem.js
+++ front-end/src/atoms/SidebarWorkspaceItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { EditIcon } from "../atoms/icons";
+import { GearSmallGreyIcon } from "../atoms/icons";
 import {trimLongText} from "../util/util";
 import "./SidebarWorkspaceItem.css";
 
@@ -25,7 +25,7 @@ function SidebarWorkspaceItem( {id, name, onEdit, selected, onClickWorkspace} ) 
         {formattedName}
       </div>
       <div className={`${componentName}-edit`} onClick={handleEditWorkspaceClick}> 
-        <EditIcon/>
+        <GearSmallGreyIcon/>
       </div>
     </div>
   );
